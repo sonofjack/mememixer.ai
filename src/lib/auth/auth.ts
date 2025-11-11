2ab5fd9313aba8dbb54b799faad29b2b09e2270b
Jackson Rowe <jackson.rowe@gmail.com>
2025-11-11 16:00:26 +0700

    init

---
+++ src/lib/auth/auth.ts
@@ -0,0 +1,49 @@
+import { createServerOnlyFn } from "@tanstack/react-start";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { reactStartCookies } from "better-auth/react-start";
+
+import { env } from "~/env/server";
+import { db } from "~/lib/db";
+
+const getAuthConfig = createServerOnlyFn(() =>
+  betterAuth({
+    baseURL: env.VITE_BASE_URL,
+    telemetry: {
+      enabled: false,
+    },
+    database: drizzleAdapter(db, {
+      provider: "pg",
+    }),
+
+    // https://www.better-auth.com/docs/integrations/tanstack#usage-tips
+    plugins: [reactStartCookies()],
+
+    // https://www.better-auth.com/docs/concepts/session-management#session-caching
+    session: {
+      cookieCache: {
+        enabled: true,
+        maxAge: 5 * 60, // 5 minutes
+      },
+    },
+
+    // https://www.better-auth.com/docs/concepts/oauth
+    socialProviders: {
+      github: {
+        clientId: env.GITHUB_CLIENT_ID!,
+        clientSecret: env.GITHUB_CLIENT_SECRET!,
+      },
+      google: {
+        clientId: env.GOOGLE_CLIENT_ID!,
+        clientSecret: env.GOOGLE_CLIENT_SECRET!,
+      },
+    },
+
+    // https://www.better-auth.com/docs/authentication/email-password
+    emailAndPassword: {
+      enabled: true,
+    },
+  }),
+);
+
+export const auth = getAuthConfig();
